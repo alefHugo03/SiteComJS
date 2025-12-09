b346250e268d0c04d08dcdbd14941e0bbfc0f3d9
Alef <alef65507@gmail.com>
2025-12-09 20:48:48 -0300

    arrumando front-end

--- api/routes/pagina/sobreRoute.js
+++ api/routes/pagina/sobreRoute.js
@@ -6,6 +6,10 @@ const routes = express.Router();
 routes.get('/sobre', GuiaControler.guiaCaminho('/templates/informacoes/sobre.html'));
 routes.get('/quemsomos', GuiaControler.guiaCaminho('/templates/informacoes/quemsomos.html'));
 
+routes.get('/cadastro/login', GuiaControler.guiaCaminho('/templates/cadastro/login.html'));
+routes.get('/cadastro/novousuario', GuiaControler.guiaCaminho('/templates/cadastro/novaconta.html'));
+routes.get('/cadastro/mudarsenha', GuiaControler.guiaCaminho('/templates/cadastro/mudarsenha.html'));
+
 routes.get('/livros', GuiaControler.guiaCaminho('/templates/biblioteca/livros.html'));
 
 export default routes
